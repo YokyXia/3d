3aa2aa5e0b31f784c7f123dbb374bc6897156d23
Alex <452549644@qq.com>
2024-01-09 16:39:39 +0800

    关卡白模初版

--- Assets/script/TransmitTo1.cs
+++ Assets/script/TransmitTo1.cs
@@ -9,8 +9,6 @@ public class TransmitTo1 : MonoBehaviour
     public GameObject transmit1_active;
     public GameObject transmit;
     public GameObject transmit_active;
-    public GameObject T_01;
-    public bool IsSaved;
     void Start()
     {
         
@@ -19,34 +17,18 @@ public class TransmitTo1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (IsSaved && Input.GetKey(KeyCode.R))
-        {
-            T_01.SetActive(false);
-
-            transmit1_active.SetActive(true);
-            GlobalData.Instance.transmit_to = 1;
-            transmit.SetActive(false);
-            transmit_active.SetActive(true);
-            transmit1.SetActive(false);
-        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Player>() != null)
         {
-            T_01.SetActive(true);
-            IsSaved = true;
-
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.GetComponent<Player>() != null)
-        {
-            IsSaved = false;
-            T_01.SetActive(false);
+            transmit1_active.SetActive(true);
+            GlobalData.Instance.transmit_to = 1;
+            transmit.SetActive(false);
+            transmit_active.SetActive(true);
+            transmit1.SetActive(false);
         }
     }
 }
